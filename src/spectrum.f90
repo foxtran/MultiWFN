1a45ddd047bc9f7cc0c31f7acbf06dcd54ec6a85
FulgurIgor <i.s.ger@ya.ru>
2020-04-18 16:02:36 +0300

    Repair spectrum.f90

--- src/spectrum.f90
+++ src/spectrum.f90
@@ -136,7 +136,7 @@ if (index(filename,"multiple.txt")/=0) then !Multiple file list with weights is 
 			if (mollegend(i)(1:1)=='$') mollegend(i)=mollegend(i)(2:)
 		end if
 		inquire(file=c200tmp,exist=alive)
-		if (alive==.false.) then
+		if (.not.alive) then
 			write(*,"(' Error: Cannot find ',a)") trim(c200tmp)
 			if (index(c200tmp,'/')/=0) then
 				write(*,*) "Reminder: Since the file path contains / symbol, you should add "" at the two ends of the path, so that the file can be properly loaded"
@@ -353,7 +353,7 @@ do while(.true.)
         read(*,"(a)") c200tmp
         if (c200tmp==" ") c200tmp="spectrum.dat"
 	    inquire(file=c200tmp,exist=alive)
-	    if (alive==.false.) then
+	    if (.not.alive) then
 	        write(*,*) "Error: Cannot find the file! Press ENTER button to return"
             read(*,*)
             cycle
